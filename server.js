b28ccc18c3eedb18e0d515dbbb855889158e0e43
BMXsanko <sanyi.mehes98@gmail.com>
2019-04-02 23:31:20 +0200

    Added play functions with delay

--- server.js
+++ server.js
@@ -9,54 +9,91 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '.', 'index.html'));
 })
 
-app.get('/start', (req, res) => {
-  player.play({
-    path: './sounds/hu/start.wav',
-  }).then((params) => {
-    res.send("done")
-  }).catch(() => {
-    res.send("file did not play!")
-  })
+let languages = {
+  hu : true,
+  sk : true,
+  ro : true
+}
+
+let speakDelay = 2000;
+
+app.get('/start', async (req, res) => {
+  if(languages.hu){
+    await player.play({path: "./sounds/hu/start.wav",sync: true})
+    await timeout()
+  }
+  if(languages.sk){
+    await player.play({path: "./sounds/sk/start.wav",sync: true})
+    await timeout()
+  }
+  if(languages.ro){
+    await player.play({path: "./sounds/ro/start.wav",sync: true})
+    await timeout()
+  }
 })
 
 app.get('/stop', (req, res) => {
-  player.play({
-    path: './sounds/stop.wav',
-  }).then((params) => {
-    res.send("done")
-  }).catch(() => {
-    res.send("file did not play!")
-  })
+  if(languages.hu){
+    await player.play({path: "./sounds/hu/stop.wav",sync: true})
+    await timeout()
+  }
+  if(languages.sk){
+    await player.play({path: "./sounds/sk/stop.wav",sync: true})
+    await timeout()
+  }
+  if(languages.ro){
+    await player.play({path: "./sounds/ro/stop.wav",sync: true})
+    await timeout()
+  }
 })
 
 app.get('/trash', (req, res) => {
-  player.play({
-    path: './sounds/trash.wav',
-  }).then((params) => {
-    res.send("done")
-  }).catch(() => {
-    res.send("file did not play!")
-  })
+  if(languages.hu){
+    await player.play({path: "./sounds/hu/trash.wav",sync: true})
+    await timeout()
+  }
+  if(languages.sk){
+    await player.play({path: "./sounds/sk/trash.wav",sync: true})
+    await timeout()
+  }
+  if(languages.ro){
+    await player.play({path: "./sounds/ro/trash.wav",sync: true})
+    await timeout()
+  }
 })
 
 app.get('/container', (req, res) => {
-  player.play({
-    path: './sounds/cont.wav',
-  }).then((params) => {
-    res.send("done")
-  }).catch(() => {
-    res.send("file did not play!")
-  })
+  if(languages.hu){
+    await player.play({path: "./sounds/hu/cont.wav",sync: true})
+    await timeout()
+  }
+  if(languages.sk){
+    await player.play({path: "./sounds/sk/cont.wav",sync: true})
+    await timeout()
+  }
+  if(languages.ro){
+    await player.play({path: "./sounds/ro/cont.wav",sync: true})
+    await timeout()
+  }
 })
 
 app.get('/szunet', (req, res) => {
-  player.play({
-    path: './sounds/szunet.wav',
-  }).then((params) => {
-    res.send("done")
-  }).catch(() => {
-    res.send("file did not play!")
-  })
+  if(languages.hu){
+    await player.play({path: "./sounds/hu/pause.wav",sync: true})
+    await timeout()
+  }
+  if(languages.sk){
+    await player.play({path: "./sounds/sk/pause.wav",sync: true})
+    await timeout()
+  }
+  if(languages.ro){
+    await player.play({path: "./sounds/ro/pause.wav",sync: true})
+    await timeout()
+  }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+function timeout() {
+  return new Promise(resolve => setTimeout(resolve, speakDelay));
+}
--- test.js
+++ test.js
@@ -1,9 +1,43 @@
 const player = require("node-wav-player")
 
-setInterval(() => {
-  player.play({path:"./sounds/start.wav"}).catch((e) => {
-    console.log(e);
-    
+// player.play({
+//     path: "./sounds/hu/start.wav",
+//     sync: true
+//   })
+//   .then(() => new Promise((resolve) => setTimeout(resolve, 2000)))
+//   .then(() => player.play({
+//     path: "./sounds/sk/start.wav",
+//     sync: true
+//   }))
+//   .then(() => new Promise((resolve) => setTimeout(resolve, 2000)))
+//   .then(() => player.play({
+//     path: "./sounds/ro/start.wav",
+//     sync: true
+//   }))
+
+
+// then(() => delay(2000, )).then(() => console.log("hello"))
+
+
+async function x(){
+  if(true){
+  await player.play({path: "./sounds/hu/start.wav",sync: true})
+  await timeout(3000)
   }
-  )
-}, 2000); 
+  if(true){
+  await player.play({path: "./sounds/sk/start.wav",sync: true})
+  await timeout(3000)
+  }
+  if(true){
+  await player.play({path: "./sounds/ro/start.wav",sync: true})
+  await timeout(3000)
+  }
+}
+
+x()
+// setTimeout((params) => {
+//   console.log("gg")
+// }
+// ,40000)
+
+
